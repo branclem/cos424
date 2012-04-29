10919d1a75582d2ac0b4e654df6090d0127e9422
Brandon Podmayersky <branclem@gmail.com>
2012-04-29 15:44:04 -0400

    Moves cross-validation to a new file.

--- project_script.R
+++ project_script.R
@@ -1,31 +1,4 @@
 
-# Run k-way cross validation on the provided data set. The API is as
-# follows: fit(training data) should fit return a model, and then
-# pred(model, test data element) should predict the response for the new data.
-cross.validate <- function(k, data, fit, pred) {
-    predictions <- rep(0, nrow(data));
-
-    # Choose the folds.
-    folds <- sample(rep(1:k, length=nrow(data)));
-
-    for (i in 1:k) {
-        # Split into out-of-fold and in-fold data.
-        train <- data[folds != i,];
-        test  <- data[folds == i,];
-
-        # Train on the other k-1 folds.
-        model <- fit(train);
-
-        for (j in 1:nrow(test)) {
-	      row <- as.integer(row.names(test[j,]));
-            predictions[row] <- pred(model, test[j,]);
-        }
-    }
-
-    return(predictions);
-}
-
-
 # Read in the dataset.
 library(foreign);
 raw.data <- read.dta(file="alcohol_data.dta");
@@ -34,7 +7,7 @@ head(raw.data);
 validation.set <- raw.data[9000:10904, ];
 data <- raw.data[1:8999,];
 
-# Sample invocation
+# Sample invocation syntax of cross-validation.
 #lm.pred <- cross.validate(5,
 #           oct,
 #           function(oct) { return(lm(octane ~ ., data=oct)) },
---
+++ validation.R
@@ -0,0 +1,26 @@
+
+# Run k-way cross validation on the provided data set. The API is as
+# follows: fit(training data) should fit return a model, and then
+# pred(model, test data element) should predict the response for the new data.
+cross.validate <- function(k, data, fit, pred) {
+    predictions <- rep(0, nrow(data));
+
+    # Choose the folds.
+    folds <- sample(rep(1:k, length=nrow(data)));
+
+    for (i in 1:k) {
+        # Split into out-of-fold and in-fold data.
+        train <- data[folds != i,];
+        test  <- data[folds == i,];
+
+        # Train on the other k-1 folds.
+        model <- fit(train);
+
+        for (j in 1:nrow(test)) {
+	      row <- as.integer(row.names(test[j,]));
+            predictions[row] <- pred(model, test[j,]);
+        }
+    }
+
+    return(predictions);
+}
