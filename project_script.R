098ce3f700647408a279f42204fd6ffd31dd9e1a
Brandon Podmayersky <branclem@gmail.com>
2012-04-28 22:18:12 -0400

    Yuliya's update to chop off the test set.

--- project_script.R
+++ project_script.R
@@ -1,6 +1,8 @@
 
 # Read in the dataset.
 library(foreign);
-data <- read.dta(file="alcohol_data.dta");
+raw.data <- read.dta(file="alcohol_data.dta");
 
-head(data);
+head(raw.data);
+validation.set <- raw.data[9000:10904, ];
+data <- raw.data[1:8999,];
