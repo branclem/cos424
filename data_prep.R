d6b535dc3c496511f436de6d66a8fecd86731b4d
Brandon Podmayersky <branclem@gmail.com>
2012-04-29 16:37:06 -0400

    Fixes data prep.

--- data_prep.R
+++ data_prep.R
@@ -7,26 +7,11 @@ raw.data <- read.dta(file="alcohol_data.dta");
 head(raw.data);
 validation.set<-raw.data[9000:10904, ];
 data<-raw.data[1:8999,];
-#data$A2[grep('female', data$A2)]<-0;
 
-data$A2<-sub("[[:graph:][:space:]]*\\(", "", data$A2);
-data$A2<-sub("\\)[[:graph:][:space:]]*", "", data$A2);
 data$A2<-as.numeric(data$A2);
-
-data$A5<-sub("[[:graph:][:space:]]*\\(", "", data$A5);
-data$A5<-sub("\\)[[:graph:][:space:]]*", "", data$A5);
 data$A5<-as.numeric(data$A5);
-
-data$D3A<-sub("[[:graph:][:space:]]*\\(", "", data$D3A);
-data$D3A<-sub("\\)[[:graph:][:space:]]*", "", data$D3A);
 data$D3A<-as.numeric(data$D3A);
-
-data$D3B<-sub("[[:graph:][:space:]]*\\(", "", data$D3B);
-data$D3B<-sub("\\)[[:graph:][:space:]]*", "", data$D3B);
 data$D3B<-as.numeric(data$D3B);
-
-data$C1<-sub("[[:graph:][:space:]]*\\(", "", data$C1);
-data$C1<-sub("\\)[[:graph:][:space:]]*", "", data$C1);
 data$C1<-as.numeric(data$C1);
 
 #na.action = na.omit
@@ -41,9 +26,9 @@ data.omitted<-data.omitted[!is.na(data.omitted$C1),]
 
 fit.linear<-lm(C1~A1+A2+A5+D3A+D3B, data.omitted, na.action=na.omit)
 fit.glm<-glm(C1~A1+A2+A5+D3A+D3B, gaussian, data.omitted, na.action=na.omit)
-summary(fit)
+summary(fit.glm)
 
-plot.new()
-points(y=fit.linear$fitted.values, x = data.omitted$A1,  col='red')
+plot(y=fit.linear$fitted.values, x = data.omitted$A1,  col='red')
 points(y=data.omitted$C1, x=data.omitted$A1, col='green')
 points(y=fit.glm$fitted.values, x = data.omitted$A1,  col='orange')
+
