97076c4d83161e7859b03836f77d3f120c2d438b
Brandon Podmayersky <branclem@gmail.com>
2012-04-30 21:00:09 -0400

    multimix now uses the proper syntax for getting the training/test data.

--- multmix.R
+++ multmix.R
@@ -4,8 +4,7 @@ library(mixtools);
 source('data_prep.R');
 source('validation.R');
 
-D <- get.data();
-data <- D$data;
+data <- get.data.numeric();
 
 raws<-cbind(data$C1, data$A1, data$A2, data$A5, data$D3A, data$D3B)
 #multmix.data<-makemultdata(raws, cuts = median(c(data$C1, data$A1, data$A2, data$A5, data$D3A, data$D3B)))
